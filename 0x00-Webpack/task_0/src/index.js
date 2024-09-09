7740db336634904e148811077b4c2731d3e09a0b
rachael-ojo <adeolaatilade@gmail.com>
2024-09-09 02:22:05 +0000

    Updated

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('body').append('<p>First paragraph added with jQuery.</p>');
+  $('body').append('<p>Second paragraph added with jQuery.</p>');
+  $('body').append('<p>Third paragraph added with jQuery.</p>');
+});
