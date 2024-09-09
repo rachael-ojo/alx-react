497e2ee1d3ba61a74055208806c9f086d48cd83e
rachael-ojo <adeolaatilade@gmail.com>
2024-09-09 12:55:29 +0000

    Updated

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -2,6 +2,7 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
+  console.log('jQuery is working!');
   $('body').append('<p>First paragraph added with jQuery.</p>');
   $('body').append('<p>Second paragraph added with jQuery.</p>');
   $('body').append('<p>Third paragraph added with jQuery.</p>');
