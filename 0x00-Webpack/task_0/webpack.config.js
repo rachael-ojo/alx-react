43172d89d1102cdfec3811917651d0415cfae6d1
rachael-ojo <adeolaatilade@gmail.com>
2024-09-09 16:29:38 +0000

    Updated

--- 0x00-Webpack/task_0/webpack.config.js
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  mode: 'development'
 };
