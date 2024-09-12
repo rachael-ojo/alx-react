4ac171719ead9c699d3d7f80cc7c3d9b826fae77
rachael-ojo <adeolaatilade@gmail.com>
2024-09-12 07:14:50 +0000

    Updated

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,9 +1,5 @@
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
+import { Map } from 'immutable';
 
-const immutableObj = getImmutableObject(obj);
-console.log(immutableObj); // Output: Map { "fear": true, "smell": -1033575916.9145899, "wall": false, "thing": -914767132 }
+export function getImmutableObject(obj) {
+  return Map(obj);
+}
