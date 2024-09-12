cc24bd72d71ab00fe42d867e23084be6ce99e2bf
rachael-ojo <adeolaatilade@gmail.com>
2024-09-12 15:18:03 +0000

    Updated

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,23 +1,5 @@
-function myFunction() {
-  console.log("Hello, World!");
-}
+import { Map, fromJS } from 'immutable';
 
-if (typeof myFunction === 'function') {
-  console.log("myFunction is defined.");
-  myFunction();
-} else {
-  console.log("myFunction is not defined.");
-}
-
-if (myFunction instanceof Function) {
-  console.log("myFunction is a function.");
-  myFunction();
-} else {
-  console.log("myFunction is not a function.");
-}
-
-try {
-  myFunction();
-} catch (e) {
-  console.log("myFunction is not defined.");
+export function getImmutableObject(obj) {
+  return fromJS(obj);
 }
