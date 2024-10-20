5afdb7d02c980065b1a80358f89fbc11618b79a7
Rachael Ojo <adeolaatilade@gmail.com>
2024-10-20 21:30:41 +0100

    Updated

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-	  <img src={logo} alt="Holberton Logo" width="200px" height="200px" />
+          < img src={logo} alt="" />
         <h1 style={{ color: 'red' }}>School dashboard</h1>
       </header>
-      <h2 style={{ backgroundColor: 'red', height: '4px', width: '100%', display: 'block', marginBottom: '10px' }}></h2>
+      <h2 style={{ backgroundColor: 'red', height: '4px', marginTop: '1px', width: '100%', display: 'inline-block', marginBottom: '10px' }}></h2>
       <div className="App-body">
-        <p style={{ textAlign: 'left', marginBottom: '500px' }}>Login to access the full dashboard</p>
-        <h2 style={{ backgroundColor: 'red', height: '4px', width: '100%', display: 'block', marginBottom: '10px' }}></h2>
+        <p style={{ textAlign: 'left', paddingLeft: '40px', marginTop: '60px', marginBottom: '500px' }}>Login to access the full dashboard</p>
+        <h2 style={{ backgroundColor: 'red', height: '4px', width: '100%', display: 'inline-block', marginBottom: '10px' }}></h2>
       </div>
       <footer className="App-footer">
         <p>Copyright 2023 - holberton School</p>
