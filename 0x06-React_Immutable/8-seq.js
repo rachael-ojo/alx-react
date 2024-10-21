057f78bc888c350471cdde144f3b78aa58f122c9
Rachael Ojo <adeolaatilade@gmail.com>
2024-10-21 21:32:41 +0100

    Updated

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,11 @@
 import { Seq } from 'immutable';
 
-export function printBestStudents(grades) {
-  const bestStudents = Seq(grades)
-    .filter(student => student.score >= 70)
-    .map(student => ({
-      firstName: capitalizeFirstLetter(student.firstName),
-      lastName: capitalizeFirstLetter(student.lastName),
-    }));
+const myFunction = (arg) => {
+  // Use parentheses around arrow function arguments
+  const seq = Seq(arg);
+  // Define variables and functions before using them
+  const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+  return seq.map(capitalizeFirstLetter);
+};
 
-  bestStudents.forEach(student => {
-    console.log(`${student.firstName} ${student.lastName}`);
-  });
-}
-
-function capitalizeFirstLetter(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
+export default myFunction;
