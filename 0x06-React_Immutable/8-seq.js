334b4654ee40bbfff92d027df8f55badb69ea724
Rachael Ojo <adeolaatilade@gmail.com>
2024-10-21 21:34:33 +0100

    Updated

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,11 +1,13 @@
 import { Seq } from 'immutable';
 
-const myFunction = (arg) => {
-  // Use parentheses around arrow function arguments
-  const seq = Seq(arg);
-  // Define variables and functions before using them
-  const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-  return seq.map(capitalizeFirstLetter);
+// Define the function first to avoid "use before define" errors
+const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+// Main function that uses Seq
+const processSeq = (input) => {
+  // Use parentheses around the arrow function argument
+  return Seq(input).map((item) => capitalizeFirstLetter(item));
 };
 
-export default myFunction;
+// Use default export as the file has a single export
+export default processSeq;
